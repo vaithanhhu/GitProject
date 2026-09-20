592ac9e648d5924cc93a7c84a757d531635188f3
Hai <hadmeo18@gmail.com>
2026-09-20 19:17:21 +0700

    Add responsive navbar exercise

---
+++ bai-responsive-navbar/script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const menuIcon = document.querySelector(".menu-icon");
+    const navLinks = document.querySelector(".nav-links");
+
+    menuIcon.addEventListener("click", function () {
+        navLinks.classList.toggle("active");
+    });
+});
